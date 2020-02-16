0e8469f04414b982f26558d3529c74972f06145f
Leyang Zou <leyang.zou@student.tdsb.on.ca>
2020-02-15 21:43:23 -0500

    Update BFS.cpp

--- algorithms/BFS.cpp
+++ algorithms/BFS.cpp
@@ -1,37 +1,39 @@
-#include <iostream>
-#include <vector>
-#include <climits>
+// 15/02/2020 - documented code due to requests for "meaningful" comments
 
-using namespace std;
+#include <iostream> // This line includes iostream
+#include <vector> // This line includes vector
+#include <climits> // This line includes climits
 
-int n = 10000;
-vector<int> edges[10000];
+using namespace std; // This line specifies namespace std
 
-int BFS(int iS, int iE) {
-    
-    vector<int> queue;
-    queue.push_back(iS);
-    int dist[n] = {0};
-    for(int i = 1; i < n; i++) dist[i] = INT_MAX;
-    
-    while(!queue.empty()) {
-        
-        int node = queue.front();
-        queue.erase(queue.begin());
-        
-        for(int i = 0; i < edges[node].size(); i++) {
-            
-            if(dist[node] + 1 < dist[edges[node].at(i)]) {
-                
-                queue.push_back(dist[edges[node].at(i)]);
-                dist[edges[node].at(i)] = dist[node] + 1;
-                
-            }
-            
-        }
-        
-    }
-    
-    return dist[iE] == INT_MAX ? -1 : dist[iE];
-    
-}
+int n = 10000; // This line defines an integer n
+vector<int> edges[10000]; // This line defines a vector of integers called edges
+// This line is an empty space
+int BFS(int iS, int iE) { // This line creates a function called BFS with two integer parameters
+// This line is an empty space
+    vector<int> queue; // This line creates a vector of integers
+    queue.push_back(iS); // This line pushes iS into queue
+    int dist[n] = {0}; // This line creates an array of integers of size n
+    for(int i = 1; i < n; i++) dist[i] = INT_MAX; // This line sets all elements of dist from 1 to n to INT_MAX
+// This line is an empty space
+    while(!queue.empty()) { // This line defines a while loop that functions while queue is not empty
+// This line is an empty space
+        int node = queue.front(); // This line assigns the front of the queue to node
+        queue.erase(queue.begin()); // This line erases the front of the queue
+// This line is an empty space
+        for(int i = 0; i < edges[node].size(); i++) { // This line defines a for loop going from 0 to edges[node].size()
+// This line is an empty space
+            if(dist[node] + 1 < dist[edges[node].at(i)]) { // This line checks if dist[node] + 1 < dist[edges[node].at(i)]
+// This line is an empty space
+                queue.push_back(dist[edges[node].at(i)]); // This line pushes dist[edges[node].at(i)] into the queue
+                dist[edges[node].at(i)] = dist[node] + 1; // This line sets dist[edges[node].at(i)] to dist[node] + 1  
+// This line is an empty space
+            } // This line is a curly bracket
+// This line is an empty space    
+        } // This line is another curly bracket
+// This line is an empty space
+    } // This line is yet another curly bracket
+// This line is an empty space
+    return dist[iE] == INT_MAX ? -1 : dist[iE]; // This line does things
+// This line is an empty space
+} // This line is yet ANOTHER curly bracket
