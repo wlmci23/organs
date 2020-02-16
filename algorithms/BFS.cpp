95422d443879257f2bea30dcb6da188e59a951d0
Leyang Zou <leyang.zou@student.tdsb.on.ca>
2020-02-15 21:51:05 -0500

    Update BFS.cpp

--- algorithms/BFS.cpp
+++ algorithms/BFS.cpp
@@ -6,27 +6,28 @@
 // This line is an empty space
 using namespace std; // This line specifies namespace std
 // This line is an empty space
-int n = 10000; // This line defines an integer n
-vector<int> edges[10000]; // This line defines a vector of integers called edges
+int someIntegerNThatRepresentsTheNumberOfNodes = 10000; // This line defines an integer n
+vector<int> edgesThatRepresentPathsOnGraph[10000]; // This line defines a vector of integers called edges
 // This line is an empty space
-int BFS(int iS, int iE) { // This line creates a function called BFS with two integer parameters
+unsigned long long int doBreadthFirstSearchOnGraph(int startingNodeOnGraph, int endingNodeOnGraph) { // This line creates a function called BFS with two integer parameters
 // This line is an empty space
-    vector<int> queue; // This line creates a vector of integers
-    queue.push_back(iS); // This line pushes iS into queue
-    int dist[n] = {0}; // This line creates an array of integers of size n
-    for(int i = 1; i < n; i++) dist[i] = INT_MAX; // This line sets all elements of dist from 1 to n to INT_MAX
+    vector<int> aQueueThatRepresentsTheNodesInQueue; // This line creates a vector of integers
+    aQueueThatRepresentsTheNodesInQueue.push_back(startingNodeOnGraph); // This line pushes iS into queue
+    int distanceOnGraphFromStartingNodeOnGraphToEndingNodeOnGraph[someIntegerNThatRepresentsTheNumberOfNodes] = {0}; // This line creates an array of integers of size n
+    for(int i = 0; i < someIntegerNThatRepresentsTheNumberOfNodes; i++) distanceOnGraphFromStartingNodeOnGraphToEndingNodeOnGraph[i] = INT_MAX; // This line sets all elements of dist from 1 to n to INT_MAX
+    distanceOnGraphFromStartingNodeOnGraphToEndingNodeOnGraph[startingNodeOnGraph] = 0; // This line sets dist[iS] to 0 
 // This line is an empty space
-    while(!queue.empty()) { // This line defines a while loop that functions while queue is not empty
+    while(!aQueueThatRepresentsTheNodesInQueue.empty()) { // This line defines a while loop that functions while queue is not empty
 // This line is an empty space
-        int node = queue.front(); // This line assigns the front of the queue to node
-        queue.erase(queue.begin()); // This line erases the front of the queue
+        int node = aQueueThatRepresentsTheNodesInQueue.front(); // This line assigns the front of the queue to node
+        aQueueThatRepresentsTheNodesInQueue.erase(aQueueThatRepresentsTheNodesInQueue.begin()); // This line erases the front of the queue
 // This line is an empty space
-        for(int i = 0; i < edges[node].size(); i++) { // This line defines a for loop going from 0 to edges[node].size()
+        for(int i = 0; i < edgesThatRepresentPathsOnGraph[node].size(); i++) { // This line defines a for loop going from 0 to edges[node].size()
 // This line is an empty space
-            if(dist[node] + 1 < dist[edges[node].at(i)]) { // This line checks if dist[node] + 1 < dist[edges[node].at(i)]
+            if(distanceOnGraphFromStartingNodeOnGraphToEndingNodeOnGraph[node] + 1 < distanceOnGraphFromStartingNodeOnGraphToEndingNodeOnGraph[edgesThatRepresentPathsOnGraph[node].at(i)]) { // This line checks if dist[node] + 1 < dist[edges[node].at(i)]
 // This line is an empty space
-                queue.push_back(dist[edges[node].at(i)]); // This line pushes dist[edges[node].at(i)] into the queue
-                dist[edges[node].at(i)] = dist[node] + 1; // This line sets dist[edges[node].at(i)] to dist[node] + 1  
+                aQueueThatRepresentsTheNodesInQueue.push_back(dist[edgesThatRepresentPathsOnGraph[node].at(i)]); // This line pushes dist[edges[node].at(i)] into the queue
+                distanceOnGraphFromStartingNodeOnGraphToEndingNodeOnGraph[edgesThatRepresentPathsOnGraph[node].at(i)] = distanceOnGraphFromStartingNodeOnGraphToEndingNodeOnGraph[node] + 1; // This line sets dist[edges[node].at(i)] to dist[node] + 1  
 // This line is an empty space
             } // This line is a curly bracket
 // This line is an empty space    
@@ -34,6 +35,6 @@ int BFS(int iS, int iE) { // This line creates a function called BFS with two in
 // This line is an empty space
     } // This line is yet another curly bracket
 // This line is an empty space
-    return dist[iE] == INT_MAX ? -1 : dist[iE]; // This line does things
+    return distanceOnGraphFromStartingNodeOnGraphToEndingNodeOnGraph[endingNodeOnGraph] == INT_MAX ? -1 : distanceOnGraphFromStartingNodeOnGraphToEndingNodeOnGraph[endingNodeOnGraph]; // This line does things
 // This line is an empty space
 } // This line is yet ANOTHER curly bracket
