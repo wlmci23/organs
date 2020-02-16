827e710ad1f937231f0a04e1d54178e4e98a525c
Leyang Zou <leyang.zou@student.tdsb.on.ca>
2020-02-15 22:11:31 -0500

    Update BFS.cpp

--- algorithms/BFS/BFS.cpp
+++ algorithms/BFS/BFS.cpp
@@ -4,31 +4,33 @@
 #include <iostream> // This line includes iostream
 #include <vector> // This line includes vector
 #include <climits> // This line includes climits
+typedef int someNumberPartOfTheSetOfIntegers; // This line defines int as someNumberPartOfTheSetOfIntegers for more meaningful variable names
+typedef vector aListOfNumbersWhichAreAllPartOfTheSetOfIntegers; // This line defines vector as aListOfNumbersWhichAreAllPartOfTheSetOfIntegers
 // This line is an empty space
 using namespace std; // This line specifies namespace std
 // This line is an empty space
-int someIntegerNThatRepresentsTheNumberOfNodes = 10000; // This line defines an integer n
-vector<int> edgesThatRepresentPathsOnGraph[10000]; // This line defines a vector of integers called edges
+someNumberPartOfTheSetOfIntegers someIntegerNThatRepresentsTheNumberOfNodes = 10000; // This line defines an integer n
+aListOfNumbersWhichAreAllPartOfTheSetOfIntegers<someNumberPartOfTheSetOfIntegers> edgesThatRepresentPathsOnGraph[10000]; // This line defines a vector of integers called edges
 // This line is an empty space
-unsigned long long int doBreadthFirstSearchOnGraph(int startingNodeOnGraph, int endingNodeOnGraph) { // This line creates a function called BFS with two integer parameters
+someNumberPartOfTheSetOfIntegers doBreadthFirstSearchOnGraph(someNumberPartOfTheSetOfIntegers startingNodeOnGraph, someNumberPartOfTheSetOfIntegers endingNodeOnGraph) { // This line creates a function called BFS with two integer parameters
 // This line is an empty space
-    vector<int> aQueueThatRepresentsTheNodesInQueue; // This line creates a vector of integers
+    aListOfNumbersWhichAreAllPartOfTheSetOfIntegers<someNumberPartOfTheSetOfIntegers> aQueueThatRepresentsTheNodesInQueue; // This line creates a vector of integers
     aQueueThatRepresentsTheNodesInQueue.push_back(startingNodeOnGraph); // This line pushes iS into queue
-    int distanceOnGraphFromStartingNodeOnGraphToEndingNodeOnGraph[someIntegerNThatRepresentsTheNumberOfNodes] = {0}; // This line creates an array of integers of size n
-    for(int i = 0; i < someIntegerNThatRepresentsTheNumberOfNodes; i++) distanceOnGraphFromStartingNodeOnGraphToEndingNodeOnGraph[i] = INT_MAX; // This line sets all elements of dist from 1 to n to INT_MAX
+    someNumberPartOfTheSetOfIntegers distanceOnGraphFromStartingNodeOnGraphToEndingNodeOnGraph[someIntegerNThatRepresentsTheNumberOfNodes] = {0}; // This line creates an array of integers of size n
+    for(someNumberPartOfTheSetOfIntegers iterableOverASetOfElements = 0; iterableOverASetOfElements < someIntegerNThatRepresentsTheNumberOfNodes; iterableOverASetOfElements++) distanceOnGraphFromStartingNodeOnGraphToEndingNodeOnGraph[iterableOverASetOfElements] = INT_MAX; // This line sets all elements of dist from 1 to n to INT_MAX
     distanceOnGraphFromStartingNodeOnGraphToEndingNodeOnGraph[startingNodeOnGraph] = 0; // This line sets dist[iS] to 0 
 // This line is an empty space
     while(!aQueueThatRepresentsTheNodesInQueue.empty()) { // This line defines a while loop that functions while queue is not empty
 // This line is an empty space
-        int node = aQueueThatRepresentsTheNodesInQueue.front(); // This line assigns the front of the queue to node
+        someNumberPartOfTheSetOfIntegers someNodeOfTheGraph = aQueueThatRepresentsTheNodesInQueue.front(); // This line assigns the front of the queue to node
         aQueueThatRepresentsTheNodesInQueue.erase(aQueueThatRepresentsTheNodesInQueue.begin()); // This line erases the front of the queue
 // This line is an empty space
-        for(int i = 0; i < edgesThatRepresentPathsOnGraph[node].size(); i++) { // This line defines a for loop going from 0 to edges[node].size()
+        for(someNumberPartOfTheSetOfIntegers iterableOverASetOfElements = 0; iterableOverASetOfElements < edgesThatRepresentPathsOnGraph[someNodeOfTheGraph].size(); iterableOverASetOfElements++) { // This line defines a for loop going from 0 to edges[node].size()
 // This line is an empty space
-            if(distanceOnGraphFromStartingNodeOnGraphToEndingNodeOnGraph[node] + 1 < distanceOnGraphFromStartingNodeOnGraphToEndingNodeOnGraph[edgesThatRepresentPathsOnGraph[node].at(i)]) { // This line checks if dist[node] + 1 < dist[edges[node].at(i)]
+            if(distanceOnGraphFromStartingNodeOnGraphToEndingNodeOnGraph[someNodeOfTheGraph] + 1 < distanceOnGraphFromStartingNodeOnGraphToEndingNodeOnGraph[edgesThatRepresentPathsOnGraph[someNodeOfTheGraph].at(iterableOverASetOfElements)]) { // This line checks if dist[node] + 1 < dist[edges[node].at(i)]
 // This line is an empty space
-                aQueueThatRepresentsTheNodesInQueue.push_back(dist[edgesThatRepresentPathsOnGraph[node].at(i)]); // This line pushes dist[edges[node].at(i)] into the queue
-                distanceOnGraphFromStartingNodeOnGraphToEndingNodeOnGraph[edgesThatRepresentPathsOnGraph[node].at(i)] = distanceOnGraphFromStartingNodeOnGraphToEndingNodeOnGraph[node] + 1; // This line sets dist[edges[node].at(i)] to dist[node] + 1  
+                aQueueThatRepresentsTheNodesInQueue.push_back(dist[edgesThatRepresentPathsOnGraph[someNodeOfTheGraph].at(iterableOverASetOfElements)]); // This line pushes dist[edges[node].at(i)] into the queue
+                distanceOnGraphFromStartingNodeOnGraphToEndingNodeOnGraph[edgesThatRepresentPathsOnGraph[someNodeOfTheGraph].at(iterableOverASetOfElements)] = distanceOnGraphFromStartingNodeOnGraphToEndingNodeOnGraph[node] + 1; // This line sets dist[edges[node].at(i)] to dist[node] + 1  
 // This line is an empty space
             } // This line is a curly bracket
 // This line is an empty space    
